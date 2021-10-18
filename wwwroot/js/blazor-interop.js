cbd7e7be11cc794249e277399972638cb474e54f
glowies <comuokta@gmail.com>
2021-10-17 21:55:42 -0400

    feat: full-screen canvas with resizing...

---
+++ wwwroot/js/blazor-interop.js
@@ -0,0 +1,10 @@
+﻿function AddResizeListener(elementId, dotnetRef, callbackMethodName) {
+    const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            var width = entry.contentRect.width;
+            var height = entry.contentRect.height;
+            dotnetRef.invokeMethodAsync(callbackMethodName, width, height);
+        }
+    });
+    resizeObserver.observe(document.getElementById(elementId));
+}
